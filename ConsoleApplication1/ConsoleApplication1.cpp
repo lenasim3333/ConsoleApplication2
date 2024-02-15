d45796024caeee858f7a02a633d74cea0a90800d
AkimenkoAV <AkimenkoAV@C7A107-8NEW>
2024-02-15 17:41:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+   cout << "Vvedite a,b!\n";
+   cin >> a >> b;
+   c = a + b;
+   cout << " " << c;
+
+}
+
